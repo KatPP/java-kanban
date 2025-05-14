dacfc76c79acec6c4326f3f3b115783251e19449
zenit1925 <kostia-85-85@yandex.ru>
2025-05-14 11:05:27 +0300

    4 - Update Task 14/05/2025

--- src/Task.java
+++ src/Task.java
@@ -1,28 +1,29 @@
 import java.util.Objects;
 
 public class Task {
-    private final int id;  // сделал поле final
-    private String nameTask;
+    private final int id;
+    private String name;
     private String description;
-    private Status statusTask;
+    private Status status;
 
-    public Task(int id, String nameTask, String description, Status statusTask) {
+    public Task(int id, String name, String description, Status status) {
         this.id = id;
-        this.nameTask = nameTask;
+        this.name = name;
         this.description = description;
-        this.statusTask = statusTask;
+        this.status = status;
     }
 
+    // Геттеры и сеттеры
     public int getId() {
         return id;
     }
 
-    public String getNameTask() {
-        return nameTask;
+    public String getName() {
+        return name;
     }
 
-    public void setNameTask(String nameTask) {
-        this.nameTask = nameTask;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getDescription() {
@@ -33,12 +34,12 @@ public class Task {
         this.description = description;
     }
 
-    public Status getStatusTask() {
-        return statusTask;
+    public Status getStatus() {
+        return status;
     }
 
-    public void setStatusTask(Status statusTask) {
-        this.statusTask = statusTask;
+    public void setStatus(Status status) {
+        this.status = status;
     }
 
     @Override
@@ -46,21 +47,20 @@ public class Task {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Task task = (Task) o;
-        return id == task.id;  // сравниваем только по id
+        return id == task.id;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id);  // хэш-код только по id
+        return Objects.hash(id);
     }
 
     @Override
     public String toString() {
         return "Task{" +
                 "id=" + id +
-                ", nameTask='" + nameTask + '\'' +
-                ", description='" + description + '\'' +
-                ", statusTask=" + statusTask +
+                ", name='" + name + '\'' +
+                ", status=" + status +
                 '}';
     }
 }
