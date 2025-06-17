ab6644f3b04e58923d01de9e44ab63b0aa1767f8
zenit1925 <kostia-85-85@yandex.ru>
2025-06-17 16:01:48 +0300

    18 - Чекстайл ошибка TaskManager

--- src/main/java/tracker/service/TaskManager.java
+++ src/main/java/tracker/service/TaskManager.java
@@ -9,28 +9,45 @@ import java.util.List;
 
 public interface TaskManager {
     List<Task> getHistory(); // <- Новый метод
+
     // Методы для Task
     List<Task> getAllTasks();
+
     void deleteAllTasks();
+
     Task getTask(int id);
+
     Task createTask(String name, String description, Status status);
+
     void updateTask(Task task);
+
     void deleteTask(int id);
 
     // Методы для Subtask
     List<Subtask> getAllSubtasks();
+
     void deleteAllSubtasks();
+
     Subtask getSubtask(int id);
+
     Subtask createSubtask(String name, String description, Status status, int epicId);
+
     void updateSubtask(Subtask subtask);
+
     void deleteSubtask(int id);
 
     // Методы для Epic
     List<Epic> getAllEpics();
+
     void deleteAllEpics();
+
     Epic getEpic(int id);
+
     Epic createEpic(String name, String description);
+
     void updateEpic(Epic epic);
+
     void deleteEpic(int id);
+
     List<Subtask> getEpicSubtasks(int epicId);
 }
