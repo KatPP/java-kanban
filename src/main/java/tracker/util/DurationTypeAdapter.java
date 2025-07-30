c90ef53613d2b724bef749d5558bf3227bddd5e1
zenit1925 <kostia-85-85@yandex.ru>
2025-07-30 08:05:57 +0300

    26 - Спринт 9. Add LocalDateTimeTypeAdapter, DurationTypeAdapter

---
+++ src/main/java/tracker/util/DurationTypeAdapter.java
@@ -0,0 +1,28 @@
+package tracker.util;
+
+import com.google.gson.TypeAdapter;
+import com.google.gson.stream.JsonReader;
+import com.google.gson.stream.JsonWriter;
+import java.io.IOException;
+import java.time.Duration;
+
+public class DurationTypeAdapter extends TypeAdapter<Duration> {
+
+    @Override
+    public void write(JsonWriter out, Duration duration) throws IOException {
+        if (duration == null) {
+            out.nullValue();
+        } else {
+            out.value(duration.toMillis());
+        }
+    }
+
+    @Override
+    public Duration read(JsonReader in) throws IOException {
+        if (in.peek() == com.google.gson.stream.JsonToken.NULL) {
+            in.nextNull();
+            return null;
+        }
+        return Duration.ofMillis(in.nextLong());
+    }
+}
